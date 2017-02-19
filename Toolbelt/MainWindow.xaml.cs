461b153b0134e37d8d93e43031aad8c411732b18
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2017-02-19 01:10:58 +0100

    Added jpc.de image download

---
+++ Toolbelt/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
+
+namespace Toolbelt
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void GetImages(object sender, EventArgs e)
+        {
+            try
+            {
+                var path = TxtBrowse.Text;
+                var productIds = TxtProductIds.Text
+                    .Split(new[] {Environment.NewLine}, StringSplitOptions.None)
+                    .Where(s => !string.IsNullOrWhiteSpace(s))
+                    .ToList();
+                TxtProductIds.Text = string.Join(Environment.NewLine, productIds);
+                var eans = TxtEans.Text
+                    .Split(new[] {Environment.NewLine}, StringSplitOptions.None)
+                    .Where(s => !string.IsNullOrWhiteSpace(s))
+                    .ToList();
+                TxtEans.Text = string.Join(Environment.NewLine, eans);
+
+                if (string.IsNullOrWhiteSpace(path))
+                {
+                    MessageBox.Show(
+                        "No output directory set",
+                        "Error",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                    return;
+                }
+
+                Directory.CreateDirectory(path);
+
+                if (!productIds.Any())
+                {
+                    MessageBox.Show(
+                        "No product ids",
+                        "Error",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                    return;
+                }
+
+                if (!eans.Any())
+                {
+                    MessageBox.Show(
+                        "No EANs",
+                        "Error",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                    return;
+                }
+
+                if (productIds.Count != eans.Count)
+                {
+                    MessageBox.Show(
+                        "Uneven number of productIds and EANs",
+                        "Error",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                    return;
+                }
+
+                var dict = productIds.Zip(eans, (k, v) => new {k, v}).ToDictionary(x => x.k, x => x.v);
+
+                var baseUrl = @"http://media2.jpc.de/image/w600/front/0/";
+                using (var client = new WebClient())
+                {
+                    foreach (var kvp in dict)
+                    {
+                        var url = $"{baseUrl}{kvp.Value}.jpg";
+                        var dl = Path.Combine(path, $"{kvp.Key}.jpg");
+                        client.DownloadFile(url, dl);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                        ex.Message,
+                        "Error",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+            }
+        }
+
+        private void testFileBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            TxtBrowse.Text = fbd.SelectedPath;
+        }
+    }
+}
